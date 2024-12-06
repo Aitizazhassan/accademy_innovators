51511f6b9b8c9d43000ae4cfdba0deaa81c9b79b
aitizaz <aitiali.hassan@gmail.com>
2024-12-06 14:31:07 -0500

    dply

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git checkout development
-    git reset --hard origin/development
-    git pull origin development
+    # git checkout development
+    # git reset --hard origin/development
+    # git pull origin development
     
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader
